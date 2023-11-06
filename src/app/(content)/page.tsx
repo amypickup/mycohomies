35a963565690a3a8397a7ce38665b2d50d4d5745
Amy Pickup <amy.pickup@gmail.com>
2023-11-06 15:49:14 -0500

    make padding a lil cuter

--- src/app/(content)/page.tsx
+++ src/app/(content)/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
   return (
     <div className="text-center">
       <div className="bg-gradient-to-r from-red-400 via-orange-400 to-amber-300 border-b border-zinc-800">
-        <div className="italic md:p-5 text-3xl md:text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-violet-600 via-blue-400 to-green-400">
+        <div className="italic p-2 md:p-5 text-3xl md:text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-violet-600 via-blue-400 to-green-400">
           mycohomies
         </div>
       </div>
@@ -20,12 +20,12 @@ export default async function Home() {
           className="w-full z-0 object-center object-cover pointer-events-none"
           layout="fill"
         />
-        <div className="relative background-transparent z-1 py-32 px-0 md:px-32">
-          <div className="max-w-xl mx-auto bg-gradient-to-r from-red-400 via-orange-400 to-amber-300 border border-black ">
+        <div className="relative background-transparent z-1 py-40 px-0 md:px-32">
+          <div className="max-w-xl mx-auto bg-gradient-to-r from-red-400 via-orange-400 to-amber-300 border border-black p-3 md:p-8">
             <iframe
               src="https://mycohomies.substack.com/embed"
               height="320"
-              className="border-black bg-white mx-auto my-5 w-full"
+              className="border-black bg-white mx-auto w-full"
             ></iframe>
           </div>
         </div>
